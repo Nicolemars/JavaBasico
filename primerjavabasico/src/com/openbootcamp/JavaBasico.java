d622b435e1e74455ac280302a4316a4d35630d96
Nicolemars <122317164+Nicolemars@users.noreply.github.com>
2023-01-19 15:25:48 -0500

    Primer ejercicio de curso java basico

---
+++ primerjavabasico/src/com/openbootcamp/JavaBasico.java
@@ -0,0 +1,41 @@
+package com.openbootcamp;
+
+public class JavaBasico {
+    public static void main(String[] args) {
+
+        //Datos numéricos enteros
+        byte  num1 = 1;
+        short num2 = 2;
+        int   num3 = 3;
+        long  num4 = 4;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
+        //Datos numéricos reales
+
+        float num5  = 5.5f;
+        double num6 = 6.6;
+
+        System.out.println(num5);
+        System.out.println(num6);
+
+        //Cadena de texto
+
+        String texto = "Texto";
+        System.out.println(texto);
+
+        //Caracter
+
+        char caracter = 'a';
+        System.out.println(caracter);
+
+        //Booleano
+
+        boolean isTrue = false;
+        System.out.println(isTrue);
+
+    }
+}
